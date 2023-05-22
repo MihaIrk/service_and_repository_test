ecc19d533c35e2d8fdf848373b0794e0880bef13
ymka1205 <ymka1205@mail.ru>
2023-05-22 11:27:29 +0800

    Изменил логику

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:service_and_repository_test/services/todo_db.dart';
-import 'package:service_and_repository_test/services/web_service.dart';
+import 'package:service_and_repository_test/screens/home_page.dart';
 
-import 'models/todo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,95 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Service and Repository Test Project',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<ToDo> allToDo = [];
-
-  @override
-  void initState() {
-    getValues();
-    super.initState();
-  }
-
-  void getValues () async {
-    allToDo = await ToDoDataBase.instance.readAllToDo();
-  }
-
-  void _incrementCounter() async {
-    final twentyToDo = await ToDoDataBase.instance.readToDo(20);
-    print(twentyToDo);
-    final newToDo = ToDo(localId:twentyToDo.localId,userId: twentyToDo.userId, id: twentyToDo.id, title: 'k12345678', completed: twentyToDo.completed);
-    await ToDoDataBase.instance.updateToDO(newToDo);
-    final testUpdateTodo = await ToDoDataBase.instance.readToDo(20);
-    print(testUpdateTodo);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-              onPressed: () async {
-                List<ToDo> toDos = await RealWebService().getToDoFromWeb();
-                for (var element in toDos) {
-                  ToDoDataBase.instance.createToDo(element);
-                }
-                setState(() {
-                });
-              },
-              icon: Icon(Icons.cloud_download))
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: allToDo.length,
-              itemBuilder: (context, index) => ToDoViewWidget(toDo: allToDo[index],)),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'get',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-
-class ToDoViewWidget extends StatelessWidget {
-  const ToDoViewWidget({Key? key, required this.toDo}) : super(key: key);
-  final ToDo toDo;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: Text('${toDo.localId}'),
-        title: Text(toDo.title),
-        trailing: toDo.completed ? Icon(Icons.check) : Icon(Icons.do_not_disturb),
-      ),
+      home: const MyHomePage(),
     );
   }
 }
--- lib/services/web_service.dart
+++ lib/services/web_service.dart
@@ -1,28 +1,38 @@
 import 'package:dio/dio.dart';
 import 'package:service_and_repository_test/models/todo.dart';
-
-import '../models/todo.dart';
+import 'package:service_and_repository_test/services/todo_db.dart';
 
 abstract class WebService {
   Future <List<ToDo>> getToDoFromWeb();
+  Future loadToDoInBaseFromWeb();
   Future addToDoInWeb(ToDo toDo);
 }
 
-
-
 class RealWebService extends WebService {
-  final dio = Dio();
+  final _dio = Dio();
   @override
   Future<List<ToDo>> getToDoFromWeb() async {
-    final response = await dio.get('https://jsonplaceholder.typicode.com/todos');
+    final response = await _dio.get('https://jsonplaceholder.typicode.com/todos');
     final testList = response.data as List<dynamic>;
-    final List<ToDo> todo = testList.map((e) => ToDo.fromJson(e as Map<String, dynamic>)).toList();
-    return todo;
+    final List<ToDo> toDos = testList.map((e) => ToDo.fromJson(e as Map<String, dynamic>)).toList();
+    return toDos;
+  }
+
+  @override
+  Future loadToDoInBaseFromWeb() async {
+    final response = await _dio.get('https://jsonplaceholder.typicode.com/todos');
+    final testList = response.data as List<dynamic>;
+    final List<ToDo> toDos = testList.map((e) => ToDo.fromJson(e as Map<String, dynamic>)).toList();
+    for (var element in toDos) {
+      ToDoDataBase.instance.createToDo(element);
+    }
   }
 
   @override
   Future addToDoInWeb(ToDo toDo) async {
-    final request = await dio.post('https://jsonplaceholder.typicode.com/todos');
+    final request = await _dio.post('https://jsonplaceholder.typicode.com/todos');
     print(request.data);
   }
 }
+
+
